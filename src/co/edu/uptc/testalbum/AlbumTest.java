04a2881ce59b88f7fa870856415bcaf2064a5ab4
JuancDuarte <juan.duarte09@uptc.edu.co>
2023-11-18 22:12:14 -0500

    test

---
+++ src/co/edu/uptc/testalbum/AlbumTest.java
@@ -0,0 +1,33 @@
+package co.edu.uptc.testalbum;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import co.edu.uptc.control.AlbumControl;
+public class AlbumTest {
+	@Test
+	void test() {
+		AlbumControl ac = new AlbumControl();
+		String name, cover, description, genre, nam, cov, desc, gen;
+		int year, month, date, hours, minutes, seconds;
+		name = "The Eminem Show";
+		cover = "https://images.app.goo.gl/xnx54QJA2im3eGvo6";
+		description = "Album musical de Eminem";
+		genre = "Hip Hop";
+		year = 2002;
+		month = 05;
+		date = 26;
+		hours = 1;
+		minutes = 17;
+		seconds = 00;
+		nam = name;
+		cov = cover;
+		desc = description;
+		gen = genre;
+		assertEquals(nam, name);
+		assertEquals(cov, cover);
+		assertEquals(desc, description);
+		assertEquals(gen, genre);
+	}
+}
